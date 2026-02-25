260d645a9a81eddedaa7189977e7b1b0c4e84f64
saiswarup <saiswarup825@gmail.com>
2026-02-25 20:36:28 +0530

    Completed

--- backend/Joblisting/settings.py
+++ backend/Joblisting/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+CORS_ALLOWED_ORIGINS = ["https://joblisting-gold.vercel.app"]
+CSRF_TRUSTED_ORIGINS = ["https://joblisting-gold.vercel.app"]
